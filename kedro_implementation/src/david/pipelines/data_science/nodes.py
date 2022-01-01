bd433fb80a123225b8d39ce89e601f325d1d8f8b
Matthew Christian <2010.mbc@gmail.com>
2022-01-01 11:51:36 -0500

    removing dead libs

--- kedro_implementation/src/david/pipelines/data_science/nodes.py
+++ kedro_implementation/src/david/pipelines/data_science/nodes.py
@@ -1,13 +1,10 @@
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing
-from sklearn.preprocessing import scale
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
-from kedro.extras.datasets.matplotlib import MatplotlibWriter
 from kedro.extras.datasets.text import TextDataSet
 
 
