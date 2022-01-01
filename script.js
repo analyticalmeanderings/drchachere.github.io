36e94267ba9fe2033774d12bf3afe8ed6935db68
Matthew Christian <2010.mbc@gmail.com>
2022-01-01 10:54:13 -0500

    making a list, checking it twice

--- script.js
+++ script.js
@@ -51,26 +51,6 @@ const dataRegistry = (function () {
     // TODO: build_map() modeled after https://www.kenan-flagler.unc.edu/programs/undergraduate-business/global-programs/
   };
 
-  // TODO: add functionality to sort table
-  function filterTable(table_name, local_input) {
-    var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById(local_input);
-    filter = input.value.toUpperCase();
-    table = document.getElementById(table_name);
-    tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
-        }
-      }
-    }
-  }
-
 
 
   function build_table(input_location, id_target) {
