64a7ed97ab851202adc48ebcefeec3e2f79f4a4c
Matthew Christian <2010.mbc@gmail.com>
2022-01-01 10:39:26 -0500

    removing old code

--- script.js
+++ script.js
@@ -50,29 +50,6 @@ const dataRegistry = (function () {
     // build_alerts();
     // TODO: build_map() modeled after https://www.kenan-flagler.unc.edu/programs/undergraduate-business/global-programs/
   };
-  /* When the user clicks on the button,
-  toggle between hiding and showing the dropdown content */
-  // TODO: rename myFunction to something meaningful
-  function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-
-  // TODO: rename Function to something meaningful
-  function filterFunction() {
-    var input, filter, ul, li, a, i;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    div = document.getElementById("myDropdown");
-    a = div.getElementsByTagName("a");
-    for (i = 0; i < a.length; i++) {
-      txtValue = a[i].textContent || a[i].innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        a[i].style.display = "";
-      } else {
-        a[i].style.display = "none";
-      }
-    }
-  }
 
   // TODO: add functionality to sort table
   function filterTable(table_name, local_input) {
