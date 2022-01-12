cf0eef1f299a784e61c54a974c1157e9f46a598f
Matthew Christian <2010.mbc@gmail.com>
2022-01-12 14:27:57 -0500

    show me

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-import data from "../data/a.json" assert { type: "json" };
+// import data from "../data/a.json" assert { type: "json" };
 
-window.onload = function() {
+// window.onload = function() {
  
 
-  let text = "<table>";
-  // console.log(data.length);
-  // console.log(Object.keys(data[0]).length);
-  for (const [i, value] of data.entries()) {
+//   let text = "<table>";
+//   // console.log(data.length);
+//   // console.log(Object.keys(data[0]).length);
+//   for (const [i, value] of data.entries()) {
 
-    text+="<tr>"
+//     text+="<tr>"
 
-    for (const col of (Object.keys(value))) {
+//     for (const col of (Object.keys(value))) {
 
-      if (i===0) {
-        console.log(col);
-        text+="<th>"+col+"</th>"
+//       if (i===0) {
+//         console.log(col);
+//         text+="<th>"+col+"</th>"
 
-      }
-    }
-    text+="</tr>"
+//       }
+//     }
+//     text+="</tr>"
 
-}
+// }
 
-  text += "</table>";
-  document.getElementById("shortage_model").innerHTML = text;
-  console.log(text)
+//   text += "</table>";
+//   document.getElementById("shortage_model").innerHTML = text;
+//   console.log(text)
 
-}
+// }
